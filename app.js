5352ff7063051bb30e85508c32a3a27ecedbc16f
StaxGit <lario.free@gmail.com>
2020-03-24 20:23:55 +0100

    Add exercise creation post route

--- app.js
+++ app.js
@@ -30,7 +30,6 @@ app.post("/api/exercise/new-user", middleware.checkUsername, function(req, res) 
 	//create new user and generate a new id for said user
 	let newUser = {
 		username: req.body.username,
-		shortId: shortid.generate()
 	}
 	//save user to the DB
 	User.create(newUser, function(err, createdUser) {
@@ -38,10 +37,98 @@ app.post("/api/exercise/new-user", middleware.checkUsername, function(req, res) 
 			console.log(err);
 		} else {
 			//return json with username, id and shortid
-			res.json({"username": createdUser.username, "_id": createdUser._id, "shortId": createdUser.shortId});
+			res.json({"username": createdUser.username, "_id": createdUser._id});
 		}
 	});
 });
+
+// let exerciseSchema = new mongoose.Schema({
+// 	description: String,
+// 	duration: Number,
+// 	date: {
+// 		type: Date,
+// 		default: Date.now,
+// 	},
+// 	user: {
+// 		id: {
+// 			type: mongoose.Schema.Types.ObjectId,
+// 			ref: "User"
+// 		},
+// 		username: String,
+// 	}
+// });
+
+app.post("/api/exercise/add", function(req, res) {
+	//find user by id
+	User.findById(req.body.userId, function(err, foundUser) {
+		if (err) {
+			console.log(err);
+		} else {
+			//create the new exercise object
+			let tempObject = {
+				description: req.body.description,
+				duration: req.body.duration,
+				date: req.body.date || Date.now(),
+				user: {
+					id: foundUser._id,
+					username: foundUser.username
+				}
+			}
+			//create new exercise
+			Exercise.create(tempObject, function(err, createdExercise) {
+				if (err) {
+					console.log(err);
+				} else {
+					//add exercise to user's exercise array
+					foundUser.exercises.push(createdExercise);
+					foundUser.save();
+					console.log(foundUser);
+					//return json with userid, user shortid, username, exercise duration, exercise description, exercise date
+					res.json({
+						"_id": foundUser._id, 
+						"username": foundUser.username, 
+						"duration": createdExercise.duration, 
+						"description": createdExercise.description,
+						"date": createdExercise.date,
+						"total exercises": foundUser.exercises.length
+					});
+				}
+			})
+		}
+	});
+});
+
+// //Comments Create
+// router.post("/", middleware.isLoggedIn, function(req, res) {
+// 	//find campground by id
+// 	Campground.findById(req.params.id, function(err, campground) {
+// 		if (err) {
+// 			req.flash("error", "Campground not found.")
+// 			console.log(err);
+// 		} else {
+// 			//create new comment
+// 			Comment.create(req.body.comment, function(err, comment) {
+// 				if (err) {
+// 					req.flash("error", "Something went wrong.")
+// 					console.log(err);
+// 				} else {
+// 					//add username and ID to comment
+// 					comment.author.id = req.user._id;
+// 					comment.author.username = req.user.username;
+// 					comment.save();
+// 					//connect new comment to campground
+// 					campground.comments.push(comment);
+// 					campground.save();
+// 					console.log(comment);
+// 					//redirect to showpage of said campground
+// 					req.flash("success", "Successfully added a new comment.")
+// 					res.redirect("/campgrounds/" + campground._id);
+// 				}
+// 			})
+
+// 		}
+// 	})
+// });
 
 
 
